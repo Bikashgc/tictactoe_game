e7b0578b314ad0a8bb2c351b23416f014e729339
Bikash GC <bikashgc937@gmail.com>
2023-02-23 23:58:36 +0545

    designed ui and added functionality to add cross and circle while playing game

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+// to add icon image
+  var circle = Icon(Icons.circle_outlined);
+  var cross = Icon(Icons.cancel_outlined);
+  var edit = Icon(Icons.edit_outlined);
+
+  bool isCross = true;
+  late String message;
+  late List<String> gameState;
+
+  //initialize box with empty value
+  @override
+  void initState() {
+    gameState = List.filled(9, "empty");
+    this.message = "";
+    // TODO: implement initState
+    super.initState();
+  }
+
+  //to play game
+  playGame(int index) {
+    if (this.gameState[index] == "empty") ;
+    setState(() {
+      if (this.isCross) {
+        this.gameState[index] = "cross";
+      } else {
+        this.gameState[index] = "circle";
+      }
+      isCross = !isCross;
+    });
+  }
+
+  //to reset game
+  resetGame() {
+    gameState = List.filled(9, "empty");
+    this.message = "";
+  }
+
+  // to get icon image
+  getIcon(String title) {
+    switch (title) {
+      case ('empty'):
+        return edit;
+        break;
+
+      case ('circle'):
+        return circle;
+        break;
+
+      case ('cross'):
+        return cross;
+        break;
+    }
+  }
+
+  //to check for win
+  checkWin() {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                // childAspectRatio: 1.0,
+                // mainAxisSpacing: 2,
+                // crossAxisSpacing: 2,
+              ),
+              itemBuilder: ((context, i) => SizedBox(
+                    width: 10,
+                    height: 10,
+                    child: MaterialButton(
+                      onPressed: () {
+                        this.playGame(i);
+                      },
+                      child: getIcon(this.gameState[i]),
+                    ),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
