d940358e4bf99c674f08fe44e09cbba6252e3c50
Bikash GC <bikashgc937@gmail.com>
2023-02-24 21:00:34 +0545

    solved RangeError (index) error in the code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -28,21 +28,25 @@ class _HomePageState extends State<HomePage> {
 
   //to play game
   playGame(int index) {
-    if (this.gameState[index] == "empty") ;
-    setState(() {
-      if (this.isCross) {
-        this.gameState[index] = "cross";
-      } else {
-        this.gameState[index] = "circle";
-      }
-      isCross = !isCross;
-    });
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        if (this.isCross) {
+          this.gameState[index] = "cross";
+        } else {
+          this.gameState[index] = "circle";
+        }
+        isCross = !isCross;
+      });
+    }
   }
 
   //to reset game
   resetGame() {
-    gameState = List.filled(9, "empty");
-    this.message = "";
+    setState(() {
+      gameState = List.filled(9, "empty");
+      this.message = "";
+      this.isCross = true;
+    });
   }
 
   // to get icon image
@@ -81,16 +85,19 @@ class _HomePageState extends State<HomePage> {
               padding: EdgeInsets.all(10),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
-                // childAspectRatio: 1.0,
-                // mainAxisSpacing: 2,
-                // crossAxisSpacing: 2,
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
               ),
+              itemCount: gameState.length,
               itemBuilder: ((context, i) => SizedBox(
-                    width: 10,
-                    height: 10,
+                    width: 100,
+                    height: 100,
                     child: MaterialButton(
                       onPressed: () {
-                        this.playGame(i);
+                        if (i < gameState.length) {
+                          this.playGame(i);
+                        }
                       },
                       child: getIcon(this.gameState[i]),
                     ),
