4dcfef2402b2ce064f70a8a7f94224af89ac3d59
Bikash GC <bikashgc937@gmail.com>
2023-02-25 09:11:55 +0545

    Added functionality to check who wins eiher cross or circle and displaying message or results who wins the game

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -36,6 +36,7 @@ class _HomePageState extends State<HomePage> {
           this.gameState[index] = "circle";
         }
         isCross = !isCross;
+        checkWin();
       });
     }
   }
@@ -67,7 +68,61 @@ class _HomePageState extends State<HomePage> {
   }
 
   //to check for win
-  checkWin() {}
+  checkWin() {
+    if ((gameState[0] != "empty") &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      setState(() {
+        this.message = this.gameState[0] + " Wins";
+      });
+    } else if ((gameState[3] != "empty") &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        this.message = this.gameState[3] + " Wins";
+      });
+    } else if ((gameState[6] != "empty") &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[6] + " Wins";
+      });
+    } else if ((gameState[0] != "empty") &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        this.message = this.gameState[0] + " Wins";
+      });
+    } else if ((gameState[1] != "empty") &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        this.message = this.gameState[1] + " Wins";
+      });
+    } else if ((gameState[2] != "empty") &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[2] + " Wins";
+      });
+    } else if ((gameState[0] != "empty") &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[0] + " Wins";
+      });
+    } else if ((gameState[2] != "empty") &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        this.message = this.gameState[2] + " Wins";
+      });
+    } else if (!gameState.contains("empty")) {
+      setState(() {
+        this.message = "Game Draw";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -104,6 +159,7 @@ class _HomePageState extends State<HomePage> {
                   )),
             ),
           ),
+          Text(message),
           RaisedButton(
             onPressed: () {
               resetGame();
